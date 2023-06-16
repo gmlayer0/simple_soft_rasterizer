0e210452caba29642c7229eff0654f38cc8fa332
DESKTOP-415OV3U\dofingert <dofingert@gmail.com>
2023-06-17 00:06:28 +0800

    bouding box 测试

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
     }
 
     SDL_Window *window = SDL_CreateWindow(
-            "DL SoftRender", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            "SoftRender Previewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
             WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI
     ); // 创建SDL窗口
 
@@ -33,22 +33,27 @@ int main() {
     SDL_Texture *tex = SDL_CreateTexture(render, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, WIDTH, HEIGHT);
     SDL_Event windowEvent; // SDL窗口事件
 
-    char *fb = static_cast<char *>(malloc(WIDTH * HEIGHT * 4));
+    char *fb = static_cast<char *>(malloc((WIDTH + 10) * HEIGHT * 4));
 
+    int _i = 0;
     while (true) {
+        int i = (_i < 0) ? (-_i) : _i;
         if (SDL_PollEvent(&windowEvent)) { // 对当前待处理事件进行轮询
             if (SDL_QUIT == windowEvent.type) { // 如果事件为推出SDL，结束循环
                 std::cout << "SDL quit!!" << std::endl;
                 break;
             }
         }
-        Vertex in[3] = {{{0.1f, 390.5f,  0.f, 1.f}, {0.f, 0.f}},
-                        {{0.f,  0.21f,   0.f, 1.f}, {0.f, 0.f}},
-                        {{799.5f,  599.81f, 0.f, 1.f}, {0.f, 0.f}}};
+        Vertex in[3] = {{{149.f + i,     549.f - i / 4, 0.f, 1.f}, {0.f, 0.f}},
+                        {{149.f + i / 7, 0.0f + i,      0.f, 1.f}, {0.f, 0.f}},
+                        {{549.f + i / 5, 549.f - i,     0.f, 1.f}, {0.f, 0.f}}};
+        memset(fb, 0x55, WIDTH * HEIGHT * 4);
         half_space_rasterizer(in, WIDTH, HEIGHT, nullptr, reinterpret_cast<unsigned int *>(fb));
         SDL_UpdateTexture(tex, nullptr, fb, WIDTH * 4);
         SDL_RenderCopy(render, tex, nullptr, nullptr);
         SDL_RenderPresent(render);
+        _i++;
+        if (_i > 500) _i = -500;
     }
 
     free(fb);
